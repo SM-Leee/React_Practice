b84592e41e453e08f6d80adecd63de392f7a9b07
bitacademy-douzone <tjdals1708@naver.com>
2019-04-15 17:49:04 +0900

    ReactJS test update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import MyComponent from './MyComponent';
 import EventPractice from './EventPractice';
+import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
   render() {
@@ -11,6 +13,12 @@ class App extends Component {
       <br/>
       <br/>
       <EventPractice/>
+      <br/>
+      <br/>
+      <ValidationSample/>
+      <br/>
+      <br/>
+      <ScrollBox/>
       </React.Fragment>
     );
   }
