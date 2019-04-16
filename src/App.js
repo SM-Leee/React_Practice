82dfc77e4ab1f1515d6fa9733c0fd0f8cd508371
bitacademy-douzone <tjdals1708@naver.com>
2019-04-16 14:57:27 +0900

    LifeCycle Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
-import MyComponent from './MyComponent';
-import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
+import InteraterSample from './IteraterSample';
+import LifeCycleSample from './LifeCycleSample';
 
+function getRandomColor() {
+  const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  console.log(color)
+  return color;
+}
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+      this.setState({
+      color: getRandomColor()
+    });
+  }
   render() {
     return (
-      <React.Fragment>
-      <MyComponent/>
-      <br/>
-      <br/>
-      <EventPractice/>
-      <br/>
-      <br/>
-      <ValidationSample/>
-      <br/>
-      <br/>
-      <ScrollBox/>
-      </React.Fragment>
+      <div>
+        <ValidationSample />
+        <br />
+        <br />
+        <ScrollBox />
+        <br />
+        <br />
+        <InteraterSample />
+        <br/>
+        <br/>
+        <button onClick={this.handleClick}> 랜덤색상 </button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
     );
   }
 }
